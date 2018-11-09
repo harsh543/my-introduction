7d3dfa7714049d13f180d5b935488664c07f5501
Harsh P Bajaj <harsh.rocks@hotmail.com>
2018-11-09 17:30:20 -0600

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,8 +7,14 @@ import Image from '../components/image'
 const IndexPage = () => (
   <Layout>
     <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <p>I'm Harsh Bajaj.</p>
+    <p>I'm a Cybersecurity researcher at University of Illinois </p>
+    <p>I keep coding here as well: <a href="https://www.hackerrank.com/harshpbajaj"> HackerRank  </a> </p>
+    <p> THe last hackathon I am proud of was : <a> https://hackuiowa.hackerearth.com/sprints/hackuiowa-1/dashboard/fbab7af/submission/ </a></p>
+  <p>ANd I contribute to Open Source as well :<a href=" https://www.owasp.org/index.php/OWASP_Serverless_Top_10_Project"> OWASP</a> . 
+& <a href="http://w3af.org/">w3af (going to contribute)<a/> </p>  
+  <Link to="/page-2/">Myself</Link>
+  </div>
     <div style={{ maxWidth: '300px', marginBottom: '1.45rem' }}>
       <Image />
     </div>
