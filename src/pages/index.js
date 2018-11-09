bfbf9ec57c3e2a33dd88cbf0648ccbe6a7b65b6a
Harsh P Bajaj <harsh.rocks@hotmail.com>
2018-11-09 17:44:41 -0600

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,9 +11,9 @@ const IndexPage = () => (
     <p>I'm a Cybersecurity researcher at University of Illinois </p>
     <p>I'm a Cybersecurity researcher at University of Illinois </p>
     <p>I keep coding here as well: <a href="https://www.hackerrank.com/harshpbajaj"> HackerRank  </a> </p>
-    <p> THe last hackathon I am proud of was : <a> https://hackuiowa.hackerearth.com/sprints/hackuiowa-1/dashboard/fbab7af/submission/ </a></p>
+    <p> THe last hackathon I am proud of was : <a href="https://hackuiowa.hackerearth.com/sprints/hackuiowa-1/dashboard/fbab7af/submission/"> HackUIowa IoT project </a></p>
   <p>ANd I contribute to Open Source as well :<a href=" https://www.owasp.org/index.php/OWASP_Serverless_Top_10_Project"> OWASP</a> . 
-& <a href="http://w3af.org/">w3af (going to contribute)<a/> </p>  
+and <a href="http://w3af.org/">w3af (going to contribute)<a/> </p>  
   <Link to="/page-2/">Myself</Link>
   </div>
     <div style={{ maxWidth: '300px', marginBottom: '1.45rem' }}>
