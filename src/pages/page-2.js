e13a7698c3b9607259a767dac3a005b23adb2a16
Harsh P Bajaj <harsh.rocks@hotmail.com>
2018-11-09 18:29:32 -0600

    Update page-2.js

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -5,8 +5,8 @@ import Layout from '../components/layout'
 
 const SecondPage = () => (
   <Layout>
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
+    <h1>More about me</h1>
+    <p>I enjoy creating ideas 2</p>
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
